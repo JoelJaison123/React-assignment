181c2e14925efdea9680aa4f0a8fb96356a354d6
Joel Jaison <you@example.com>
2021-07-28 18:52:51 -0400

    GSAP transition

--- src/App.js
+++ src/App.js
@@ -32,40 +32,6 @@ export default function App() {
           { ({ match }) => <Contact show={match !== null} /> }
         </Route>
 
-        {/*<Route
-          render={({ location }) => (
-            <TransitionGroup>
-              <CSSTransition key={location.key} timeout={1000} classNames="fade">
-                <Switch location={location}>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/about" component={About} />
-                  <Route path="/projects" component={Projects} />
-                  <Route path="/contact" component={Contact} />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )}
-        />
-        
-         A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/">
-            <h1>404</h1>
-          </Route>
-        </Switch>*/}
       </div>
     </Router>
   );
